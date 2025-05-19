1c5ca32f515fccc9fe8cc60fa94e326dd9bbbfd8
Aleksander Piciga <aleksander.piciga@gmail.com>
2025-05-19 13:44:22 +0200

    scaling for mobile

--- src/components/ProfilePhoto.tsx
+++ src/components/ProfilePhoto.tsx
@@ -2,8 +2,14 @@ import Image from 'next/image';
 
 export default function ProfilePhoto() {
   return (
-    <div className="w-32 h-32 rounded-full overflow-hidden border-4 border-indigo-400 flex items-center justify-center bg-white shadow-lg">
-      <Image src="/main/profile.png" alt="Profile Photo" width={128} height={128} />
+    <div className="w-24 h-24 sm:w-32 sm:h-32 rounded-full overflow-hidden border-4 border-indigo-400 flex items-center justify-center bg-white shadow-lg">
+      <Image 
+        src="/main/profile.png" 
+        alt="Profile Photo" 
+        width={128} 
+        height={128} 
+        className="w-full h-full object-cover"
+      />
     </div>
   );
 } 
